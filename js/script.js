e310448cce5a65a5cec88212f36aa82b7c0e480e
osmanzakaria532 <osmanzakaria179@gmail.com>
2023-08-11 12:25:29 +0600

    food wagon website is complate

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-$('.').slick({
-    arrows: false,
-    dots: true,
-    autoplay: true
+$('.popular_items').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: '<a href="#" class="left_arrow"><i class="fa-solid fa-angle-left"></i></a>',
+    nextArrow: '<a href="#" class="right_arrow"><i class="fa-solid fa-angle-right"></i></a>',
+});
+
+$('.search_items').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: '<a href="#" class="left_arrow"><i class="fa-solid fa-angle-left"></i></a>',
+    nextArrow: '<a href="#" class="right_arrow"><i class="fa-solid fa-angle-right"></i></a>',
+    autoplay: true,
 });
