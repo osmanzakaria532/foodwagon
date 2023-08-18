5a560ff0c03d44b73886f1e02760f2a32ffe61bd
Osman Zakaria <osmanzakaria179@gmail.com>
2023-08-18 20:23:20 +0600

    modify

--- js/script.js
+++ js/script.js
@@ -3,6 +3,38 @@ $('.popular_items').slick({
     slidesToScroll: 1,
     prevArrow: '<a href="#" class="left_arrow"><i class="fa-solid fa-angle-left"></i></a>',
     nextArrow: '<a href="#" class="right_arrow"><i class="fa-solid fa-angle-right"></i></a>',
+    responsive : [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2,
+                arrows: false,
+                dots: true,
+                autoplay: false,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                arrows: false,
+                dots: true,
+                autoplay: true,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                autoplay: true,
+            }
+        }
+    ]
 });
 
 $('.search_items').slick({
@@ -10,5 +42,27 @@ $('.search_items').slick({
     slidesToScroll: 1,
     prevArrow: '<a href="#" class="left_arrow"><i class="fa-solid fa-angle-left"></i></a>',
     nextArrow: '<a href="#" class="right_arrow"><i class="fa-solid fa-angle-right"></i></a>',
-    autoplay: true,
+    autoplay: false,
+    responsive : [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false,
+                dots: true,
+                autoplay: false,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                autoplay: false,
+            }
+        }
+    ]
 });
